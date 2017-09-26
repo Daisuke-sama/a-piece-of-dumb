0aeda8d1b4ffee18304e6e4ede5e46ae06828438
Daisuke-sama <otaku.daisuke@gmail.com>
2017-09-26 15:40:04 +0300

    Added plugin with changes for the login page.

---
+++ wp-content/plugins/custom-login-page-143213/custom-login-page-143213.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Custom Login Page 143213
+ * Version: 1.0alpha
+ * Author: Daisuke-sama
+ * License: GPL2+
+ * Text Domain: clp1_3
+ */
+
+
+/*
+ * Adding custom scripts.
+ */
+add_action( 'login_enqueue_scripts', 'clp1_3_login_styles' );
+function clp1_3_login_styles() {
+	wp_enqueue_style( 'clp1_3-custom-stylesheet', plugin_dir_url( __FILE__ ) . 'css/login-styles.css' );
+}
+
+/*
+ * Changing an error message.
+ */
+add_filter( 'login_errors', 'clp1_3_error_message' );
+function clp1_3_error_message() {
+	return 'Well, that wasn\'t it';
+}
+
+/*
+ * Disabling notification message.
+ */
+add_filter( 'login_messages', '__return_null' );
+
+/*
+ * Stop shaking of the login form when error are happening.
+ */
+add_action( 'login_head', 'clp1_3_remove_login_shakes' );
+function clp1_3_remove_login_shakes() {
+	remove_action( 'login_head', 'wp_shake_js', 12 );
+}
+
